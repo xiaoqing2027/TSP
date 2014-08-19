09fbb7b9eed3250925c835ed94745c88a86ac0b2
maggie <maggie94gkn@gmail.com>
2014-08-18 22:55:04 -0500

    session1 src code

--- session1.py
+++ session1.py
@@ -1,24 +1,42 @@
 
 
-def exp(a,n):
+def exp1(a,n):
     if(n==0):
         return 1
-    return a * exp(a,n-1)
+    return a * exp1(a,n-1)
 
-print exp(2,0)
-print exp(2,5)
-print exp(3,3)
+print exp1(2,0)
+print exp1(2,5)
+print exp1(3,3)
 
-def exp(a,n):
+def exp2(a,n):
+    if(n==0):
+        return 1
     temp = 1
-    if(n==0):
-        return 1
     for i in range(0,n):
         temp = temp * a
     return temp
 
-print exp(2,0)
-print exp(3,5)
-print exp(4,3)
+print exp2(2,0)
+print exp2(3,5)
+print exp2(4,3)
+
+def exp3(a,n):
+    if (n==0):
+        return 1
+
+    if(n%2==0):
+        return exp3(a*a, n/2)
+    else:
+        return a*exp3(a*a, (n-1)/2)
+    return
+
+print exp3(2,0)
+print exp3(5,3)
+print exp3(2,8)
+
+
+
+
 
 
