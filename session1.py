80870fc0d363e8c05cd738ad5c086ab03ab5aba0
maggie <maggie94gkn@gmail.com>
2014-08-18 21:56:05 -0500

    session1 src code

--- session1.py
+++ session1.py
@@ -9,4 +9,16 @@ print exp(2,0)
 print exp(2,5)
 print exp(3,3)
 
+def exp(a,n):
+    temp = 1
+    if(n==0):
+        return 1
+    for i in range(0,n):
+        temp = temp * a
+    return temp
+
+print exp(2,0)
+print exp(3,5)
+print exp(4,3)
+
 
