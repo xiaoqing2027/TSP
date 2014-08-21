87c1339ba7fec901da52c0a9215de5a19c66af5e
maggie <maggie94gkn@gmail.com>
2014-08-21 01:25:14 -0500

    session1 src code

--- session1.py
+++ session1.py
@@ -29,11 +29,38 @@ def exp3(a,n):
         return exp3(a*a, n/2)
     else:
         return a*exp3(a*a, (n-1)/2)
-    return
+    
 
 print exp3(2,0)
 print exp3(5,3)
 print exp3(2,8)
+
+
+
+
+def exp4(a,n):
+    if (n==0):
+        return 1
+    if(n%2==0):
+        temp1=a*a
+        temp2=1
+        for i in range (n/2):
+            temp2= temp1*temp2
+        
+        return temp2
+    else:
+        temp3=a*a
+        temp4=1
+        for i in range((n-1)/2):
+            temp4=temp3*temp4
+        return a*temp4
+
+print exp4(2,0)
+print exp4(3,5)
+print exp4(2,7)
+print exp4(2,10)
+
+
 
 
 
