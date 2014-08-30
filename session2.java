b4cc8fec049a43b657aa588ad381acc93ca26870
Donghan Miao <miaodonghan@gmail.com>
2014-08-29 23:38:21 -0700

    session 2 starter code

---
+++ session2.java
@@ -0,0 +1,30 @@
+class Stack {
+    public void push(int element){
+    }
+    
+    public int pop(){   
+    }
+}
+
+
+class Queue {
+    public void enqueue(int element){
+    }
+    
+    public int dequeue(){
+    }
+}
+
+
+class Session2 {
+
+    public boolean pairMatches(String s){
+    
+    }
+    
+    public static void main(String []args){
+    
+        s
+    }
+
+}
---
+++ session2.py
@@ -0,0 +1,17 @@
+class Stack:
+    elements = []
+    def push(self, element):
+    def pop(self):
+    def isEmpty(self):
+
+        
+        
+def pairMatches(s):
+    return False
+
+
+
+class Queue:
+    def enqueue(self, element):
+    def dequeue(self):
+    def isEmpty(self):
