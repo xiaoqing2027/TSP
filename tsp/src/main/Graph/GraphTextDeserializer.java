797dd7f268db746a0aab52a8f39ae5eac221b671
Donghan Miao <donghan@yahoo-inc.com>
2015-11-23 15:34:44 -0600

    graph deserializer

---
+++ tsp/src/main/Graph/GraphTextDeserializer.java
@@ -0,0 +1,21 @@
+package Graph;
+
+import java.util.Scanner;
+
+public class GraphTextDeserializer {
+
+    Scanner sc = new Scanner(System.in);
+
+    public UndirectedGraph load(String file) {
+        int size = sc.nextInt();
+        UndirectedGraph g = new UndirectedGraph(size);
+        int edges = sc.nextInt();
+        for(int i = 0; i< edges; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            double distance = sc.nextDouble();
+            g.addEdge(from, to, distance);
+        }
+        return g;
+    }
+}
---
+++ tsp/src/main/Graph/Main.java
@@ -0,0 +1,9 @@
+package Graph;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
