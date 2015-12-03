602daceb2148111535b449dbc88976922c38dce3
Donghan Miao <miaodonghan@gmail.com>
2015-12-03 17:02:18 -0600

    Update GraphTextDeserializer.java

--- tsp/src/main/Graph/GraphTextDeserializer.java
+++ tsp/src/main/Graph/GraphTextDeserializer.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class GraphTextDeserializer {
 
-    Scanner sc = new Scanner(System.in);
+    Scanner sc;
 
     public UndirectedGraph load(String file) {
+        sc = new Scanner(new File(file));
         int size = sc.nextInt();
         UndirectedGraph g = new UndirectedGraph_AdjMatrix(size);
         int edges = sc.nextInt();
@@ -16,6 +17,7 @@ public class GraphTextDeserializer {
             double distance = sc.nextDouble();
             g.addEdge(from, to, distance);
         }
+        sc.close();
         return g;
     }
 }
