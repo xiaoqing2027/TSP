64ef4981f3f2cb7769e2fee457b0ec56461bbbaa
maggie <maggie94gkn@gmail.com>
2015-12-03 21:52:52 -0600

    remove line 39-43

--- tsp/src/main/Algorithms/TSP_DP.java
+++ tsp/src/main/Algorithms/TSP_DP.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package TSP;
+package Algorithms;
 
 import static java.lang.Math.pow;
 import java.util.ArrayList;
--- tsp/src/main/Algorithms/TSP_Output.java
+++ tsp/src/main/Algorithms/TSP_Output.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package TSP;
+package Algorithms;
 
 import java.io.FileNotFoundException;
 import static java.lang.System.out;
