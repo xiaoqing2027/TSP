0dfc4c5961413c7c52f8890ff2b8b6dd5d050e01
maggie <maggie94gkn@gmail.com>
2015-12-03 21:43:11 -0600

    remove line 13

--- tsp/src/main/Algorithms/TSP_Output.java
+++ tsp/src/main/Algorithms/TSP_Output.java
@@ -9,24 +9,20 @@ import java.io.FileNotFoundException;
 import static java.lang.System.out;
 import java.util.Arrays;
 
-/**
- *
- * @author miaodonghan
- */
+
 public class TSP_Output {
-    public static void main(String[] args) throws FileNotFoundException{
+
+    public static void main(String[] args) throws FileNotFoundException {
         Graph g;
-        
+
         DirectedGraphTextDeserializer gReader = new DirectedGraphTextDeserializer();
 
         g = gReader.load("./data/6.txt");
 
         TSP_DP tsp_dp = new TSP_DP(g);
         tsp_dp.computeRoutes(g);
-        //System.out.println(" "+ Arrays.toString(tsp_dp.computeRoutes(g)));
+
 //        TSPAlgorithm_BruteForce tsp_bf = new TSPAlgorithm_BruteForce(g,0);
-
 //        System.out.println(" "+ tsp_bf.solve());
-
     }
 }
